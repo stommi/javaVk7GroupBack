2f0ca5f760f3addf9106598f3cd203529227fe98
Hanna Tuomi <hannamtuomi@gmail.com>
2020-02-25 10:13:51 +0200

    first commit

---
+++ src/main/java/fi/academy/javavk7groupback/Javavk7groupbackApplication.java
@@ -0,0 +1,13 @@
+package fi.academy.javavk7groupback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javavk7groupbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javavk7groupbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fi/academy/javavk7groupback/Javavk7groupbackApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.academy.javavk7groupback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javavk7groupbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
